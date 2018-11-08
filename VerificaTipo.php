805dd532c9c29c74b0e48ca0d84cbb2026264c57
Carlos Costa <carlos.costa@deliverit.com.br>
2018-11-08 15:48:00 -0200

    Primeiro envio da classe de verificação

---
+++ VerificaTipo.php
@@ -0,0 +1,44 @@
+<?php
+
+class VerificaTipo 
+{
+    
+    public static $imagem = array('jpg', 'jpeg', 'gif', 'png');
+    public static $documento = array('doc', 'docx', 'xls', 'xlsx', 'csv');
+
+
+    /* Método que retorna o tipo de arquivo
+     * $type =  tipo de arquivo
+     * $list =  lista de arquivos aceitos
+     *
+    */
+    public static function valida($type, $list)
+    {
+        return in_array($type, $list);
+    }
+
+    /* Método específico para verificação de imagem
+     * $type =  tipo de arquivo
+     * $list =  lista de arquivos aceitos
+     *
+    */
+    public static function validaImagem($type)
+    {
+        return self::valida($type, self::$imagem);
+    }
+
+    /* Método específico para verificação de documentos
+     * $type =  tipo de arquivo
+     * $list =  lista de arquivos aceitos
+     *
+    */
+    public static function validaDocumento($type)
+    {
+        return self::valida($type, self::$imagem);
+    }
+
+}
+
+$documentoAceito = VerificaTipo::validaDocumento('jpg');
+
+echo $documentoAceito;
